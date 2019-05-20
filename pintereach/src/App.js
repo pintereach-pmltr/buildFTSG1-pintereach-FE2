bdc0081b4050674bc2d5834ecad64e28779bc88c
chrispetito <christopherpetito@gmail.com>
2019-05-20 10:38:34 -0700

    add loginpage component

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Route path='/login' component={LoginPage} />
     </div>
   );
 }
---
+++ pintereach/src/components/LoginPage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+class LoginPage extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChanges = event => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+
+    login = event => {
+        event.preventDefault();
+        this.props.login(this.state.credentials).then(() => {
+            this.props.history.push('/dashboard')
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input></input>
+                    <input></input>
+                    <button>Log In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    isLogginIn: state.isLogginIn
+})
+
+export default connect(
+    mapStateToProps,
+    { login }
+  )(LoginPage);
+  
