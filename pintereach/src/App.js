5a40ab7838dee7f3d45be2afbe64b3a458031d99
chrispetito <christopherpetito@gmail.com>
2019-05-23 17:52:53 -0700

    add styling, route

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./components/Dashboard";
 import PrivateRoute from "./PrivateRoute";
 import IndividualBoard from "./components/IndividualBoard";
 import LandingPage from "./components/Landing";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route exact path='/' component={LandingPage} />
         <Route exact path="/login" component={LoginPage} />
         <Route path='/register' component={RegisterPage} />
+        <Route path ='/user' component={UserProfile} />
         <PrivateRoute exact path='/dashboard' component={Dashboard} />
         <Route path={`/dashboard/:id`} render={(props) => <IndividualBoard {...props}/>} />
       </div>
--- pintereach/src/components/Landing.js
+++ pintereach/src/components/Landing.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import '../landing.css'
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div>
+      <div className='landing-container'>
         <div className="navbar">
           <div className="logo-box">
             {" "}
@@ -17,7 +18,9 @@ class LandingPage extends React.Component {
 
         </div>
         <div>
-            <h1 className='landing-header'>Welcome to Pintereach!</h1>
+            <h1 className='landing-header-title'>Welcome to Pintereach!</h1>
+            <p className='pg-copy'> Pintereach is a place for you to curate internet articles so you can easily access and share them at any time.</p>
+            <img src='.'></img>
         </div>
       </div>
     );
